702f8933556bf4bdcc0a5a4a0c2816612e8c7988
Seiichi Inoue <d35inou108@gmail.com>
2019-09-01 03:40:46 +0900

    added bubble sort

---
+++ sort/bubble_sort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int *array, int size) {
+    for (int i=0; i<size; ++i) {
+        for (int j=size-1; j>i; --j) {
+            if (array[j] < array[j-1]) {
+                int k = array[j];
+                array[j] = array[j-1];
+                array[j-1] = k;
+            }
+        }
+    }
+}
+
+int main() {
+    int N; cin >> N;
+    int array[N];
+    for (int i=0; i<N; ++i) cin >> array[i];
+    bubble_sort(array, N);
+    for (int i=0; i<N; ++i) cout << array[i] << " ";
+    cout << endl;
+}
