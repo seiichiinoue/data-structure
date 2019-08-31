ee64c417b6c6652873338284213c4e8fa3887518
Seiichi Inoue <d35inou108@gmail.com>
2019-09-01 03:57:43 +0900

    addeed quick sort

---
+++ sort/quick_sort.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int partition(int *array, int l, int r) {
+    int pivot = array[r];
+    int i = l-1;
+    for (int j=l; j<=r-1; ++j) {
+        if (array[j] <= pivot) {
+            ++i;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i+1], &array[r]);
+    return  i+1;
+}
+
+void quick_sort(int *array, int l, int r) {
+    if (l<r) {
+        int pivot = partition(array, l, r);
+        quick_sort(array, l, pivot-1);
+        quick_sort(array, pivot+1, r);
+    }
+}
+
+int main() {
+    int N; cin >> N;
+    int array[N];
+    for (int i=0; i<N; ++i) cin >> array[i];
+    quick_sort(array, 0, sizeof(array) / sizeof(array[0]) - 1);
+    for (int i=0; i<N; ++i) cout << array[i] << " ";
+    cout << endl;
+}
