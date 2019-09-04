e7823d9b8eb76b5e2839e52f68afeb5eb382a515
Seiichi Inoue <d35inou108@gmail.com>
2019-09-04 14:46:54 +0900

    added swap func on bubblesort

--- sort/bubble_sort.cpp
+++ sort/bubble_sort.cpp
@@ -1,13 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
 void bubble_sort(int *array, int size) {
     for (int i=0; i<size; ++i) {
         for (int j=size-1; j>i; --j) {
             if (array[j] < array[j-1]) {
-                int k = array[j];
-                array[j] = array[j-1];
-                array[j-1] = k;
+                swap(&array[j], &array[j-1]);
             }
         }
     }
