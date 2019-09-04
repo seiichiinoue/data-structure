36aa203a4c189e54c7dfef8eab50154ae616b12e
Seiichi Inoue <d35inou108@gmail.com>
2019-09-04 22:37:00 +0900

    fix

--- sort/insertion_sort.cpp
+++ sort/insertion_sort.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 void insertion_sort(int *array, int size) { 
-    for (int i=2; i<size; ++i) {
+    for (int i=1; i<size; ++i) {
         int key = array[i];
-        int j = i - 1;
-        while (j>0 && array[j]>key) {
-            array[j+1] = array[j];
-            j = j-1;
+        int j = i;
+        while (j>0 && array[j-1]>key) {
+            array[j] = array[j-1];
+            j--;
         }
-        array[j+1] = key;
+        array[j] = key;
     }
 }
 
